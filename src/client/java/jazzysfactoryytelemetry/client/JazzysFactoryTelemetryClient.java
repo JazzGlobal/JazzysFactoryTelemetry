0714e0b52dbac2ce23b536530aa2d73389a9a36b
Chris Gambrell <waymer147@gmail.com>
2026-07-10 20:38:26 -0400

    Initial Commit - Fabric mod template

---
+++ src/client/java/jazzysfactoryytelemetry/client/JazzysFactoryTelemetryClient.java
@@ -0,0 +1,10 @@
+package jazzysfactoryytelemetry.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class JazzysFactoryTelemetryClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
